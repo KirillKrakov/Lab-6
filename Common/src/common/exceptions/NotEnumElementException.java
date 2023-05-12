a4332db72a4950c8013b4071ef9d13d5fb17f94c
Krakov Kirill P3131 <111125149+KirillKrakov@users.noreply.github.com>
2023-05-12 15:36:15 +0300

    Delete Common directory

--- Common/src/common/exceptions/NotEnumElementException.java
+++
@@ -1,8 +0,0 @@
-package common.exceptions;
-
-/**
- * выбрасывается, когда введенное значение не является одним из перечисленных в Enum-классе
- */
-public class NotEnumElementException extends Exception{
-}
-
